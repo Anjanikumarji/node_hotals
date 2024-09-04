6300008551f2415411fbc981a635ddca3fa87086
Anjani Kumar <anjanikumar7973@gmail.com>
2024-09-04 23:22:03 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 //Define the MongoDB connection URL
-const mongoURL = 'mongodb://localhost:27017/hotels' //Replace 'mydatabase' with your database name
+//const mongoURL = process.env.MONGODB_URL; //Replace 'mydatabase' with your database name
+const mongoURL = process.env.MONGODB_URL; // online database
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL,{
